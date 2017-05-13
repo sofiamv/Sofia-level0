86f6763d15df1460ab37a2b8a5fd122fc0f0a7a5
Airborne4 <league@WTS-iMac-11.attlocal.net>
2017-05-13 16:02:24 -0700

    methods

---
+++ src/AnimalFarm.java
@@ -0,0 +1,77 @@
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
+
+public class AnimalFarm {
+
+	AnimalFarm() {
+		/*
+		 * 1. Ask the user which animal they want, then play the sound of that
+		 * animal.
+		 */
+		String ans = JOptionPane.showInputDialog("What sound of animal do you want?");
+
+		/* 2. Make it so that the user can keep entering new animals. */
+		String anstwo = JOptionPane.showInputDialog("What sound of *other* animal do you want?");
+		if (ans.equals("cow")){
+			playMoo();
+		}
+		if (ans.equals("duck")){
+		playQuack();
+		}
+		if (ans.equals("dog")){
+		playWoof();
+		}
+		if (anstwo.equals("cow")){
+			playMoo();
+		}
+		if (anstwo.equals("duck")){
+		playQuack();
+		}
+		if (anstwo.equals("dog")){
+		playWoof();
+		}
+	}
+	void playMoo() {
+		playNoise(mooFile);
+	}
+
+	void playQuack() {
+		playNoise(quackFile);
+	}
+
+	void playWoof() {
+		playNoise(woofFile);
+	}
+
+	  String quackFile = "/Users/League/Google Drive/league-sounds/quack.wav";
+	String mooFile = "/Users/League/Google Drive/league-sounds/moo.wav";
+	String woofFile = "/Users/League/Google Drive/league-sounds/woof.wav";
+	String meowFile = "/Users/League/Google Drive/league-sounds/meow.wav";
+	String llamaFile = "/Users/League/Google Drive/league-sounds/llama.wav";
+
+
+	/* Ignore this stuff */
+
+	public void playNoise(String soundFile) {
+    	try {
+   		 AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(soundFile));
+   		 Clip clip = AudioSystem.getClip();
+   		 clip.open(audioInputStream);
+   		 clip.start();
+   		 Thread.sleep(3400);
+    	} catch (Exception ex) {
+        	ex.printStackTrace();
+    	}
+	}
+
+	public static void main(String[] args) {
+		new AnimalFarm();
+	}
+
+}
+
+
---
+++ src/FourSquare.java
@@ -0,0 +1,53 @@
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class FourSquare {
+	
+	// 2. Create a new Robot
+	Robot john = new Robot();
+
+	void go() {
+		// 4. Make the robot move as fast as possible
+		john.setSpeed(10);
+
+		// 5. Set the pen width to 5
+		john.setPenWidth(5);
+
+		// 6. Do steps #7 to #8 four times...
+
+			// 7. Set the pen color to random
+		for (int i = 0; i < 360; i++) {
+			
+		
+		john.setRandomPenColor();
+		drawSquare();
+		john.turn(1);
+	}
+	
+
+	
+
+
+	
+			// 8. Turn the robot 90 degrees to the right
+	}
+
+	/* 3. Fill in the code to draw a square inside the method below. */
+	void drawSquare() {
+		john.penDown();
+		for (int i = 0; i < 4; i++) {
+			john.move(50);
+			john.turn(90);
+		}
+		john.penUp();
+
+		
+		
+	}
+
+	public static void main(String[] args) {
+		new FourSquare().go();
+	}
+
+}
